f00eb7b717988abc3fe04ad26b3cb90950cec72b
Felipe Almeida <felipe_almeidarj@msn.com>
2023-05-05 00:47:45 -0300

    feat: created models and serializers from apps users and addresses

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class USER_TYPE(models.TextChoices):
+    ADMIN = "Admin"
+    SELLER = "Seller"
+    CUSTOMER = "Customer"
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    type = models.CharField(
+        max_length=20,
+        choices=USER_TYPE.choices,
+        default=USER_TYPE.SELLER
+    )
+    address = models.OneToOneField("addresses.Address", on_delete=models.PROTECT)
---
+++ users/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+from users.models import User
+class UserSerializer(serializers.ModelSerializer):
+       
+    def create(self, validated_data: dict) -> User:
+        return User.objects.create_superuser(**validated_data)
+
+    def update(self, instance: User, validated_data: dict) -> User:
+        for key, value in validated_data.items():
+            if key == "password":
+                instance.set_password(value)
+            else:
+                setattr(instance, key, value)
+
+        instance.save()
+
+        return instance
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "type",
+            "address"
+        ]
+        extra_kwargs = {"password": {"write_only": True}, 
+        "username": {"validators": [UniqueValidator(queryset=User.objects.all(),
+        message="A user with that username already exists.")]}, 
+        "email": {"validators": [UniqueValidator(queryset=User.objects.all())]}}
