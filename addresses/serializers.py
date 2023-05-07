c85a9e034da61f9b72cba07846a1d6e3f3229612
Felipe Almeida <89353337+felipedsalmeida@users.noreply.github.com>
2023-05-07 11:14:46 -0300

    Revert "Feature/user address cart creation"

--- addresses/serializers.py
+++ addresses/serializers.py
@@ -2,11 +2,16 @@ from rest_framework import serializers
 
 from addresses.models import Address
 
-
 class AddressSerializer(serializers.ModelSerializer):
     def create(self, validated_data: dict) -> Address:
         return Address.objects.create(**validated_data)
-
     class Meta:
         model = Address
-        fields = ["id", "street", "number", "zip_code", "city", "state"]
+        fields = [
+            "id",
+            "street",
+            "number",
+            "zip_code",
+            "city",
+            "state"
+        ]
--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework_simplejwt import views as jwt_views
-from .views import UserView, UserIdView
-
-urlpatterns = [
-    path("users/", UserView.as_view()),
-    path("users/<int:pk>/", UserIdView.as_view()),
-    path("login/", jwt_views.TokenObtainPairView.as_view()),
-]
