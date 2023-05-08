3d85db027394097259efc1b326b3bc68e46a6189
Felipe Almeida <felipe_almeidarj@msn.com>
2023-05-08 20:56:01 -0300

    GET and POST Routes

--- products/serializers.py
+++ products/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from orders.models import Order_Products
 from products.models import Product
 
 
@@ -21,3 +22,15 @@ class ProductSerializer(serializers.ModelSerializer):
 
     def get_seller(self, obj):
         return f"{obj.seller.first_name} {obj.seller.last_name}"
+
+# class OrderProductSerializer(serializers.ModelSerializer):
+    
+#     class Meta:
+#         model = Order_Products
+#         fields = [
+#             "id",
+#             "order",
+#             "product",
+#             "amount"
+#         ]
+    
